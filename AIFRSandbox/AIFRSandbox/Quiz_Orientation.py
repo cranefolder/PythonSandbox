93cbfc12352763bd5bacf1a2de64f2a5fa10e452
Timothy Wayne Thomas <tthomas82@gatech.edu>
2017-05-11 19:30:07 -0500

    working toward problem set 3

--- AIFRSandbox/AIFRSandbox/Quiz_Orientation.py
+++ AIFRSandbox/AIFRSandbox/Quiz_Orientation.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
-# In this exercise, try to write a program that
-# will resample particles according to their weights.
-# Particles with higher weights should be sampled
-# more frequently (in proportion to their weight).
-
-# Don't modify anything below. Please scroll to the
-# bottom to enter your code.
+# In this exercise, write a program that will
+# run your previous code twice.
+# Please only modify the indicated area below!
 
 from math import *
 import random
 
-
-
 landmarks  = [[20.0, 20.0], [80.0, 80.0], [20.0, 80.0], [80.0, 20.0]]
 world_size = 100.0
-
 
 class robot:
     def __init__(self):
@@ -91,12 +84,8 @@ class robot:
             prob *= self.Gaussian(dist, self.sense_noise, measurement[i])
         return prob
 
-
-
     def __repr__(self):
         return '[x=%.6s y=%.6s orient=%.6s]' % (str(self.x), str(self.y), str(self.orientation))
-
-
 
 def eval(r, p):
     sum = 0.0;
@@ -107,50 +96,56 @@ def eval(r, p):
         sum += err
     return sum / float(len(p))
 
+#myrobot = robot()
+#myrobot.set_noise(5.0, 0.1, 5.0)
+#myrobot.set(30.0, 50.0, pi/2)
+#myrobot = myrobot.move(-pi/2, 15.0)
+#print myrobot.sense()
+#myrobot = myrobot.move(-pi/2, 10.0)
+#print myrobot.sense()
 
-
+####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER/MODIFY CODE BELOW ####
 myrobot = robot()
-myrobot = myrobot.move(0.1, 5.0)
-Z = myrobot.sense()
-
 N = 1000
+T = 10
 p = []
 for i in range(N):
     x = robot()
     x.set_noise(0.05, 0.05, 5.0)
     p.append(x)
 
-p2 = []
-for i in range(N):
-    p2.append(p[i].move(0.1, 5.0))
-p = p2
+print(eval(myrobot,p))
+for t in range(T):
+    # Move the robot and sense where it is
+    myrobot = myrobot.move(0.1, 5.0)
+    Z = myrobot.sense()
 
-w = []
-for i in range(N):
-    w.append(p[i].measurement_prob(Z))
+    # move all of the particles by the same turn and forward motion as the robot
+    p2 = []
+    for i in range(N):
+        p2.append(p[i].move(0.1, 5.0))
+    p = p2
+
+    # compute the "weight" for each particle by finding the difference of it's
+    # measurement from the robot measurement
+    w = []
+    for i in range(N):
+        w.append(p[i].measurement_prob(Z))
+
+    # resample the particles, using the weight to pull more likely particles
+    p3 = []
+    index = int(random.random() * N)
+    beta = 0.0
+    mw = max(w)
+    for i in range(N):
+        beta += random.random() * 2.0 * mw
+        while beta > w[index]:
+            beta -= w[index]
+            index = (index + 1) % N
+        p3.append(p[index])
+    p = p3
+    print(eval(myrobot,p))
+
+# print(p) #Leave this print statement for grading purposes!
 
 
-#### DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
-# You should make sure that p3 contains a list with particles
-# resampled according to their weights.
-# Also, DO NOT MODIFY p.
-
-
-
-'''
-Using the "Resampling Wheel" method to use the weights of each particle and
-create a new list with the most likely particles
-'''
-p3 = []
-index = random.randrange(0, N)
-beta = 0.0
-wmax = max(w) # there is a handy function to get maximum value from a list!
-
-for i in range(N):
-    beta += random.random() * 2 * wmax
-    while w[index] < beta:
-        beta -= w[index]
-        index = (index + 1) % N
-    p3.append(p[index])
-
-p = p3
