d3a5fd264fbc3e954a96678fe60970e8523cebdc
Timothy Wayne Thomas <tthomas82@gatech.edu>
2017-05-17 12:32:26 -0500

    Final solution for Left Turn Policy quiz

--- AIFRSandbox/AIFRSandbox/LeftTurnPolicy_Quiz.py
+++ AIFRSandbox/AIFRSandbox/LeftTurnPolicy_Quiz.py
@@ -65,23 +65,13 @@ def optimum_policy2D(grid,init,goal,cost):
             ,[[999 for col in range(len(grid[0]))] for row in range(len(grid))]
             ,[[999 for col in range(len(grid[0]))] for row in range(len(grid))]]
 
-    bestcost = [[999 for col in range(len(grid[0]))] for row in range(len(grid))]
-
     y = init[0]
     x = init[1]
     d = init[2]
     g = 0
     value[d][y][x] = g
 
-    #closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
-    closed = [[[0 for col in range(len(grid[0]))] for row in range(len(grid))]
-            ,[[0 for col in range(len(grid[0]))] for row in range(len(grid))]
-            ,[[0 for col in range(len(grid[0]))] for row in range(len(grid))]
-            ,[[0 for col in range(len(grid[0]))] for row in range(len(grid))]]
-
-    closed[d][y][x] = 1
-    path = []
-    open = [[g, y, x, d, path]]
+    open = [[g, y, x, d, policy2D]]
 
     found = False  # flag that is set when search is complete
     resign = False # flag set if we can't find expand
@@ -89,7 +79,7 @@ def optimum_policy2D(grid,init,goal,cost):
     while not found and not resign:
         if len(open) == 0:
             resign = True
-            return 'fail', value
+            return 'fail'
         else:
             open.sort()
             open.reverse()
@@ -98,44 +88,33 @@ def optimum_policy2D(grid,init,goal,cost):
             y = next[1]
             x = next[2]
             d = next[3]
-            path = list(next[4])
+            policy2D = [r[:] for r in next[4]] #needed to make a true copy
             #print('------------------------------------------------')
-            #print(next)
+            #print(next[0:4])
+            #print2dlist(policy2D)
 
             if x == goal[1] and y == goal[0]:
                 found = True
+                policy2D[y][x] = '*'
                 #print(path)
             else:
-                #for o in range(len(forward)):
                 for a in range(len(action)):
                     d2 = (d + action[a]) % len(forward)
                     motion = forward[d2]
                     y2 = y + forward[d2][0]
                     x2 = x + forward[d2][1]
                     g2 = g + cost[a]
-                    path2 = list(path)
+                    policy2D2 = [r[:] for r in policy2D] #needed to make a true copy
                     if x2 >= 0 and x2 < len(grid[0]) and y2 >=0 and y2 < len(grid):
                         if value[d2][y2][x2] > g2 and grid[y2][x2] == 0:
                             #print('a:' + str(a) + ' cost:' + str(cost[a]))
-                            path2.append(a)
-                            open.append([g2, y2, x2, d2, path2])
+                            policy2D2[y][x] = action_name[a]
+                            open.append([g2, y2, x2, d2, policy2D2])
                             value[d2][y2][x2] = g2
-                            closed[d2][y2][x2] = 1
         #print3dlist(value)
         #print2dlist(policy2D)
 
-    y = init[0]
-    x = init[1]
-    d = init[2]
-    for a in range(len(path)):
-        policy2D[y][x] = action_name[path[a]]
-        d = (d + action[path[a]]) % len(forward)
-        y = y + forward[d][0]
-        x = x + forward[d][1]
-        if x == goal[1] and y == goal[0]:
-            policy2D[y][x] = '*'
-
-    return policy2D, value
+    return policy2D
 
 def print3dlist(i):
     print('------------------------------------------------------------')
@@ -153,7 +132,7 @@ def print3dlist(i):
     print('')
 
 def print2dlist(i):
-    print('--------------    Policy              ----------------------')
+    #print('--------------    Policy              ----------------------')
     for r in range(len(i)):
         s = '['
         for c in range(len(i[0])):
@@ -165,6 +144,8 @@ def print2dlist(i):
     print('')
 
 
-result, result2 = optimum_policy2D(grid,init,goal,cost)
-print3dlist(result2)
-print2dlist(result)
+result = optimum_policy2D(grid,init,goal,cost)
+if (result == 'fail'):
+    print(result)
+else:
+    print2dlist(result)
